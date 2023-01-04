688f8dfa0c1d9cb56c9602f9342b1d4c3915d07d
CatalinBarbu <121017880+CatalinBarbu@users.noreply.github.com>
2023-01-04 13:53:42 +0100

    First Commit

---
+++ assets/JavaScript/main.js
@@ -0,0 +1,9 @@
+/* ====== Info ====== */
+let info = document.getElementById("info");
+info.innerHTML = "<h1>Hello World</h1>";
+info.innerHTML += "<h2>How are you</h2>";
+
+/* ======= container ====== */
+document.getElementById("container").innerHTML = "<p>Start of the element</p>";
+
+document.write("end of the element");
